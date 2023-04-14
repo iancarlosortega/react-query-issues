a1b92cbe63a71c4b641dfcedd7187756e2771269
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-04-14 09:47:41 -0500

    Comments of Issues

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
 	baseURL: 'https://api.github.com/repos/facebook/react',
-	headers: {},
+	headers: {
+		Authorization:
+			'Bearer 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',
+	},
 });
